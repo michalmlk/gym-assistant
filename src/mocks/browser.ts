c18de78c412f75881acef1605a075570c3451a32
michalmlk <michal.melka1@gmail.com>
2023-03-04 15:17:55 +0100

    introduce mswjs-data | create set of data

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,4 +1,10 @@
 import { setupWorker } from "msw";
 import { handlers } from './handlers';
+import { db } from './db';
 
 export const worker = setupWorker(...handlers);
+
+for(let i=0;i<25;i++) {
+    db.training.create();
+}
+
